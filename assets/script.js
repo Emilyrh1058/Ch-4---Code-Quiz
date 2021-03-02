5799c8efcec260bf98b8960d369cf9d2649be9cb
Emily Salinas <emilyrh1058@gmail.com>
2021-03-01 21:55:54 -0600

    added beginning script

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+
+// PULLED FROM W3S... NEEDS REFRACTOR
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get start time
+ 
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for seconds
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("counter").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("counter").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
