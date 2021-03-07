ed68d78149263e11a46a8e34e9a53a45ad402746
Emily Salinas <emilyrh1058@gmail.com>
2021-03-06 22:43:49 -0600

    added click listener

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
-var TimerEl
+var startBtn = document.querySelector("#beginBtn");
+function startQuiz() {
+    alert("quiz started");
+}
+
+var timer = setInterval(function() {
+    
+}
+
+
+
+startBtn.addEventListener("click");
