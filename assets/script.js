30fa350621c9e8b5c81c4f41f3e5ad4bdddcf62a
Emily Salinas <emilyrh1058@gmail.com>
2021-03-08 22:56:13 -0600

    refractor js and test

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,16 @@
 var quesIndex;
 var gameTimer;
 var timeAllotted = 30; //seconds
+var questionEl = document.getElementById("main-quiz");
+var introEl = document.querySelector(".intro");
+var timerEl = document.querySelector(".timer");
+
 
 var questionBank = [
   {
     question: "question 1",
-    answers: [
-      { ansId: "A", answer: "" },
-      { ansId: "B", answer: "" },
-      { ansId: "C", answer: "" },
-      { ansId: "D", answer: "" },
-    ],
-    correct: "A",
-    selected: null,
-    reason: "explain",
+    choices: ["A", "B", "C", "D"],
+    answer: "A",
   },
   {
     question: "question 2",
@@ -69,29 +66,45 @@ for (var i = 0; i < questionBank.length; i++) {
   console.log(questionBank[i].question);
 }
 
-function populateQuestionDetails() {
-  $("#answer-response").hide();
+function launchQuestion() {
 
-  $("#question-container").empty();
-  $("#answers-container").empty();
-  $("#answer-response").empty();
-quesIndex = 0
-  $("#question-container").html(questionBank[quesIndex].question);
-
-  var quesAnswers = questionBank[quesIndex].answers;
-
-  for (var i = 0; i < quesAnswers.length; i++) {
-    $("#answers-container").append(
-      '<div class="answer" data-content="' +
-        quesAnswers[i].ansID +
-        '">' +
-        quesAnswers[i].answer +
-        "</div>"
-    );
-  }
-
-  // renderQuesControls();
 }
 
+function init() {
+    questionEl.classList.remove("hide")
+    introEl.classList.add("hide")
+    timerEl.classList.remove("hide")
+    startTimer()
+    launchQuestion()
+}
 
-document.getElementById("beginBtn").addEventListener("click", populateQuestionDetails);
+function startTimer() {
+    var timerInterval = setInterval(function(){
+        timerEl.textContent = timeAllotted;
+        timeAllotted = timeAllotted-1;
+        if (timeAllotted === 0) {
+            clearInterval(timerInterval)
+        }
+     }, 1000);
+}
+//   quesIndex = 0;
+//   $("#question-container").html(questionBank[quesIndex].question);
+
+//   var quesAnswers = questionBank[quesIndex].answers;
+
+//   for (var i = 0; i < quesAnswers.length; i++) {
+//     $("#answers-container").append(
+//       '<div class="answer" data-content="' +
+//         quesAnswers[i].ansID +
+//         '">' +
+//         quesAnswers[i].answer +
+//         "</div>"
+//     );
+//   };
+
+  // renderQuesControls();
+
+
+document
+  .getElementById("beginBtn")
+  .addEventListener("click", init);
