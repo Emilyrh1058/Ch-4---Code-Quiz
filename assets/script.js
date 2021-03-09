8f89aedb2a08809bc6d1e3b6d363a7bfdcf34035
Emily Salinas <emilyrh1058@gmail.com>
2021-03-08 19:25:49 -0600

    refractored html and js

--- assets/script.js
+++ assets/script.js
@@ -1,84 +1,97 @@
-var queIndex;
+var quesIndex;
 var gameTimer;
-var timeAllotted = 30; //
+var timeAllotted = 30; //seconds
 
 var questionBank = [
-    {
-        question:"question 1",
-        answers:[
-            {ansId: A, answer:""},            
-            {ansId: B, answer:""},
-            {ansId: C, answer:""},
-            {ansId: D, answer:""},
-        ],
-        correct: A,
-        selected: null,
-        reason: "explain"
-    },
-    {
-        question:"question 2",
-        answers:[
-            {ansId: A, answer:""},            
-            {ansId: B, answer:""},
-            {ansId: C, answer:""},
-            {ansId: D, answer:""},
-        ],
-        correct: B,
-        selected: null,
-        reason: "explain"
-    },  
-    {
-        question:"question 3",
-        answers:[
-            {ansId: A, answer:""},            
-            {ansId: B, answer:""},
-            {ansId: C, answer:""},
-            {ansId: D, answer:""},
-        ],
-        correct: C,
-        selected: null,
-        reason: "explain"
-    },
-    {
-        question:"question 4",
-        answers:[
-            {ansId: A, answer:""},            
-            {ansId: B, answer:""},
-            {ansId: C, answer:""},
-            {ansId: D, answer:""},
-        ],
-        correct: D,
-        selected: null,
-        reason: "explain"
-    },
-    {
-        question:"question 5",
-        answers:[
-            {ansId: A, answer:""},            
-            {ansId: B, answer:""},
-            {ansId: C, answer:""},
-            {ansId: D, answer:""},
-        ],
-        correct: A,
-        selected: null,
-        reason: "explain"
-    },
+  {
+    question: "question 1",
+    answers: [
+      { ansId: "A", answer: "" },
+      { ansId: "B", answer: "" },
+      { ansId: "C", answer: "" },
+      { ansId: "D", answer: "" },
+    ],
+    correct: "A",
+    selected: null,
+    reason: "explain",
+  },
+  {
+    question: "question 2",
+    answers: [
+      { ansId: "A", answer: "" },
+      { ansId: "B", answer: "" },
+      { ansId: "C", answer: "" },
+      { ansId: "D", answer: "" },
+    ],
+    correct: "B",
+    selected: null,
+    reason: "explain",
+  },
+  {
+    question: "question 3",
+    answers: [
+      { ansId: "A", answer: "" },
+      { ansId: "B", answer: "" },
+      { ansId: "C", answer: "" },
+      { ansId: "D", answer: "" },
+    ],
+    correct: "C",
+    selected: null,
+    reason: "explain",
+  },
+  {
+    question: "question 4",
+    answers: [
+      { ansId: "A", answer: "" },
+      { ansId: "B", answer: "" },
+      { ansId: "C", answer: "" },
+      { ansId: "D", answer: "" },
+    ],
+    correct: "D",
+    selected: null,
+    reason: "explain",
+  },
+  {
+    question: "question 5",
+    answers: [
+      { ansId: "A", answer: "" },
+      { ansId: "B", answer: "" },
+      { ansId: "C", answer: "" },
+      { ansId: "D", answer: "" },
+    ],
+    correct: "A",
+    selected: null,
+    reason: "explain",
+  },
 ];
 
-function populateQuestionDetails() {
-    $("#answer-response").hide();
-
-    $("#question-container").empty();
-    $("#answers-container").empty();
-    $("#answer-response").empty();
-  
-  $("#question-container").html(questionBank[quesIndex].question);
-  
-  var quesAnswers = questionBank[quesIndex].answers;
-  
-  for (var i=0; i < quesAnswers.length; i++) {
-    $("#answers-container").append('<div class="answer" data-content="' + quesAnswers[i].ansID + '">' + quesAnswers[i].answer + '</div>');
-  }
-  
-  renderQuesControls();
+for (var i = 0; i < questionBank.length; i++) {
+  console.log(questionBank[i].question);
 }
+
+function populateQuestionDetails() {
+  $("#answer-response").hide();
+
+  $("#question-container").empty();
+  $("#answers-container").empty();
+  $("#answer-response").empty();
+quesIndex = 0
+  $("#question-container").html(questionBank[quesIndex].question);
+
+  var quesAnswers = questionBank[quesIndex].answers;
+
+  for (var i = 0; i < quesAnswers.length; i++) {
+    $("#answers-container").append(
+      '<div class="answer" data-content="' +
+        quesAnswers[i].ansID +
+        '">' +
+        quesAnswers[i].answer +
+        "</div>"
+    );
+  }
+
+  // renderQuesControls();
+}
+
+
+document.getElementById("beginBtn").addEventListener("click", populateQuestionDetails);
