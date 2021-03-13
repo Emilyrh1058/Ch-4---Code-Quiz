77368ca3fffa5c35c80e1cf6f6c4515da7dcb902
Emily Salinas <emilyrh1058@gmail.com>
2021-03-12 21:30:00 -0600

    added questions and answers and fixed html header

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,53 @@
 var questionBank = [
   {
-    question: "question 1",
-    choices: ["A", "B", "C", "D"],
-    answer: "A",
+    question: "What does HTML stand for?",
+    choices: ["HyperText Markup Language",
+              "HyperText Markup Locator", 
+              "HyperText Masking Line", 
+              "HateThis Must Leave"],
+    answer: "HyperText Markup Language",
   },
   {
-    question: "question 2",
-    choices: ["A", "B", "C", "D"],
-    answer: "B",
+    question: "To style a webpage, what language would you use?",
+    choices: ["HTML", 
+              "CSS", 
+              "Spanish", 
+              "Google"],
+    answer: "CSS",
   },
   {
-    question: "question 3",
-    choices: ["A", "B", "C", "D"],
-    answer: "C",
+    question: "What's the differenct between a <ul> and a <ol>?",
+    choices: ["underline vs overline", 
+              "undefined link vs onclick link", 
+              "unordered list vs ordered list", 
+              "universal language vs older language"],
+    answer: "unordered list vs ordered list",
   },
   {
-    question: "question 4",
-    choices: ["A", "B", "C", "D"],
-    answer: "D",
+    question: "Where should you place the link to the Javascript file?",
+    choices: ["In the title", 
+              "In the footer", 
+              "At the top of the body section", 
+              "At the bottom of the body section"],
+    answer: "At the bottom of the body section",
   },
   {
-    question: "question 4",
-    choices: ["A", "B", "C", "D"],
-    answer: "A",
+    question: "In Javascript, what doesn't var stand for?",
+    choices: ["Variable", 
+              "Vertical", 
+              "Vacation", 
+              "Vaccinate"],
+    answer: "Variable",
   },
 ];
-
-var timeAllotted = 30;
+var timeAllotted = 60;
 var questionEl = document.querySelector(".main-quiz");
 var answerEl = document.querySelector(".answers-container");
 var introEl = document.querySelector(".intro");
 var timerEl = document.querySelector(".timer");
 var buttonEl = document.querySelector(".beginBtn")
 var endEl = document.querySelector(".end")
-var questionIndex = 0
+//var questionIndex = 0
 
 function startTimer() {
     var timerInterval = setInterval(function(){
@@ -53,7 +67,6 @@ function init() {
     launchQuestion()
 }
 
-// CURRENT 
 function launchQuestion(){
   var currentQuestion = questionBank[questionIndex]
   var questionText = document.getElementById("question-container")
@@ -69,9 +82,9 @@ function launchQuestion(){
     answerEl.appendChild(choiceBtn)
   } )
 
-//   for (var i = 0; i < questionBank.length; i++) {
-//   console.log(questionBank[i].question);
-// }
+  for (var i = 0; i < questionBank.length; i++) {
+  console.log(questionBank[i].question);
+}
 }
 
 buttonEl.onclick = init
